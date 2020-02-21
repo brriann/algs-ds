c393afdd8693573ecfc930865b687a44d0bbbd27
Brian Foster <brriann@gmail.com>
2020-02-20 21:37:02 -0700

    node read file input

--- 0Intro.js
+++ 0Intro.js
@@ -1,6 +1,6 @@
 // invoke this script with an int array from CLI as "node 0Intro.js 0,1,2,3,4"
 
-if (process.argv.length < 3) {
+/* if (process.argv.length < 3) {
     console.log('usage: node 0Intro.js x,y,z,a,b,c');
 } else {
     let arrayArgument = process.argv[2].split(',');
@@ -9,4 +9,28 @@ if (process.argv.length < 3) {
     console.log('your array sum is:');
     console.log(arraySum);
 
-}
+} */
+
+const fs = require('fs');
+const readline = require('readline');
+
+
+var inputs = [];
+
+const rl = readline.createInterface({
+  input: fs.createReadStream('test2.txt'),
+  crlfDelay: Infinity
+});
+
+rl.on('line', (line) => {
+    var lineArray = [];
+    var strings = line.split(' ');
+    for (var i = 0; i < strings.length; i++) {
+        lineArray.push(Number(strings[i]));
+    }
+  inputs.push(lineArray);
+  console.log(`Line from file: ${line}`);
+}).on('close', () => {
+    console.log(inputs);
+});
+
