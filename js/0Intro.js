aebf496919d291ed1919de9a18295b924009a8b6
Brian Foster <brriann@gmail.com>
2020-02-23 16:52:13 -0700

    use js file reader

--- js/0Intro.js
+++ js/0Intro.js
@@ -1,36 +1,26 @@
-// invoke this script with an int array from CLI as "node 0Intro.js 0,1,2,3,4"
+// CLI usage: node 0Intro.js x,y,z,a,b,c
+var file = require('../tools/file.js');
 
-/* if (process.argv.length < 3) {
-    console.log('usage: node 0Intro.js x,y,z,a,b,c');
-} else {
+const INPUT_FILE_PATH = '/home/ubuntu/Dev/algs-ds/input/test1.txt'
+
+if (process.argv.length > 2) {
     let arrayArgument = process.argv[2].split(',');
     let arraySum = arrayArgument.reduce((a,b) => parseInt(a, 10) + parseInt(b, 10), 0);
     console.log(arrayArgument);
     console.log('your array sum is:');
     console.log(arraySum);
+} else {
+    file.readLinesToArray(INPUT_FILE_PATH, function (inputs) {
+        intro(inputs);
+    });
+}
 
-} */
-
-const fs = require('fs');
-const readline = require('readline');
-
-
-var inputs = [];
-
-const rl = readline.createInterface({
-  input: fs.createReadStream('test2.txt'),
-  crlfDelay: Infinity
-});
-
-rl.on('line', (line) => {
-    var lineArray = [];
-    var strings = line.split(' ');
-    for (var i = 0; i < strings.length; i++) {
-        lineArray.push(Number(strings[i]));
-    }
-  inputs.push(lineArray);
-  console.log(`Line from file: ${line}`);
-}).on('close', () => {
+function intro (inputs) {
     console.log(inputs);
-});
+    inputs.map( i => {
+        i.map(j => {
+            console.log(j);
+            })
+    });
+}
 
--- tools/file.js
+++ tools/file.js
@@ -1,23 +1,25 @@
 const fs = require('fs');
 const readline = require('readline');
 
+methods = {
+  readLinesToArray: function(filePath, callBack) {
+    var inputs = [];
+    const rl = readline.createInterface({
+      input: fs.createReadStream(filePath),
+      crlfDelay: Infinity
+    });
+    rl.on('line', (line) => {
+        var lineArray = [];
+        var strings = line.split(' ');
+        for (var i = 0; i < strings.length; i++) {
+            lineArray.push(Number(strings[i]));
+        }
+      inputs.push(lineArray);
+    }).on('close', () => {
+        callBack(inputs);
+    });
+  }
+}
 
-var inputs = [];
-
-const rl = readline.createInterface({
-  input: fs.createReadStream('test2.txt'),
-  crlfDelay: Infinity
-});
-
-rl.on('line', (line) => {
-    var lineArray = [];
-    var strings = line.split(' ');
-    for (var i = 0; i < strings.length; i++) {
-        lineArray.push(Number(strings[i]));
-    }
-  inputs.push(lineArray);
-  console.log(`Line from file: ${line}`);
-}).on('close', () => {
-    console.log(inputs);
-});
+module.exports = methods;
 
