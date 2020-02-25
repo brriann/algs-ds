5ae57af464d826c6cfa438b73dcac9c141b2298a
Brian Foster <brriann@gmail.com>
2020-02-24 20:43:08 -0700

    js unionfind client

--- js/1unionfind.js
+++ js/1unionfind.js
@@ -1,59 +1,72 @@
+// CLI usage: 
+// node 1unionfind.js n/p-q/p-q/p-q/p-q
+//
+// File usage:
+// n
+// p q
+// p q
+// p q
 
-function takeCommandLineInput() {
-    const readline = require("readline");
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output:process.stdout
-    });
+//
+// Simple client for Unionfind implementations that follow
+// Read an array size "n"
+// Read rows of elt's "p" and "q"
+//
 
-    rl.question("Enter n: ", function(n) {
-        rl.question("Enter p: ", function(p) {
-            rl.question("Enter q: ", function(q) {
-                rl.close();
-                return [n, p, q];
-            });
-        });
-    });
-}
+var file = require('/home/ubuntu/Dev/algs-ds/tools/file.js');
+var cli = require('/home/ubuntu/Dev/algs-ds/tools/cli.js');
 
-function TakeFileInput() {
-    return [10, 5, 5];
-}
+const INPUT_FILE_PATH = '/home/ubuntu/Dev/algs-ds/input/1unionfind2.txt'
 
 class unionfind { 
     constructor(n) {
         this.n = n;
     }
     union(p, q) {
-        // TODO: implement union
+        // This class is simply the UF client.
     }
 
     connected(p, q) {
-        return p === q;
+        return false;
     }
 
     find(p) {
-
+        // This class is simply the UF client.
     }
 
     count() {
+        // This class is simply the UF client.
+    }
+}
 
+function runClient (inputs) {
+    let n = inputs[0][0];
+    let uf = new unionfind(n);
+
+    for (let i = 1; i < inputs.length; i++) {
+        var p = inputs[i][0];
+        var q = inputs[i][1];
+
+        if (!uf.connected(p, q)) { 
+            uf.union(p, q);
+            console.log(p + " <---> " + q);
+        } else {
+            console.log("(" + p + " and " + q + " are connected.)");
+        }
     }
 }
 
 //
-// Program starts here.
+// CLIENT
 //
 
-var npq = takeCommandLineInput();
-
-var uf = new unionfind(npq[0]);
-var p = npq[1];
-var q = npq[2];
-
-if (!uf.connected(p, q)) { 
-    uf.union(p, q);
-    console.log(p + " " + q);
-}
-
-
+if (process.argv.length > 2) {
+    cli.readArgTo2DArray(process.argv[2], function (cliInput) {
+         runClient(cliInput);
+    });
+ 
+ } else {
+     file.readLinesTo2DArray(INPUT_FILE_PATH, function (fileInput) {
+         runClient(fileInput);
+     });
+ }
