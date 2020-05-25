850c0a7638743907750635defa699358f1807db9
Brian Foster <brriann@gmail.com>
2020-05-24 19:14:37 -0600

    queue and stack, resizing array, java

---
+++ java1/QueueResizingArray.java
@@ -0,0 +1,124 @@
+// CLI usage: 
+// java StackLinkedList 1-2-3-777-4-5-6-777-777-777
+//
+// File usage:
+// 1 2 3 777 4 5 6 777 777 777
+
+//
+// Stack of ints implemented by linked list
+// ints in input stream will be pushed onto stack
+// input of 777 will print popped int from stack
+//
+
+package java1;
+
+import java.util.Arrays;
+
+public class QueueResizingArray {
+
+    public static String INPUT_FILE_PATH = "/home/ubuntu/Dev/algs-ds/input/9stack2.txt";
+    
+    private int[] queueArray;
+    private int N = 0;
+    private int first;
+    private int last;
+
+    public QueueResizingArray() {
+        queueArray = new int[1];
+    }
+
+    public void enQueue(int item) {
+        if (N == queueArray.length) {
+            resize(2 * queueArray.length);
+        }
+        queueArray[last++] = item;
+        if (last == queueArray.length) {
+            last = 0;
+        }
+        N++;
+    }
+
+    public int deQueue() {
+        int item = queueArray[first];
+        queueArray[first] = -1; // using primitive int's instead of Integer.... -1 will be 'null' value
+        N--;
+        first++;
+        if (first == queueArray.length) {
+            first = 0;
+        }
+        if (N > 0 && N == queueArray.length / 4) {
+            resize(queueArray.length / 2);
+        }
+        return item;
+    }
+
+    private void resize(int capacity) {
+        if (capacity >= N) {
+            int[] copyArray = new int[capacity];
+            for (int i = 0; i < N; i++) {
+                copyArray[i] = queueArray[(first + i) % queueArray.length];
+            }
+            queueArray = copyArray;
+            first = 0;
+            last = N;
+        }
+        
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public int[] queueValues() {
+        if (isEmpty() || size() == 0) {
+            return new int[0];
+        } else {
+            int[] queueValues = new int[size()];
+            for (int i = 0; i < N; i++) {
+                queueValues[i] = queueArray[first + i];
+            }
+            return queueValues;
+        }
+    }
+
+    //
+    // CLIENT
+    //
+
+    private static void runClient(int[][] inputs) {
+
+        QueueResizingArray qra = new QueueResizingArray();
+
+        for (int i : inputs[0]) {
+            if (i == 777) {
+                System.out.println(String.format("dequeued from queue: %d", qra.deQueue()));
+                System.out.println(String.format("Queue size: %d", qra.size()));
+            } else {
+                qra.enQueue(i);
+                System.out.println(String.format("Enqueued: %d", i));
+                System.out.println(String.format("Queue size: %d", qra.size()));
+            }
+        }
+        int finalSize = qra.size();
+        if (finalSize > 0) {
+            System.out.println("Final queue (first-last/oldest-newest):");
+            System.out.println(Arrays.toString(qra.queueValues()));
+        } else if (qra.isEmpty()) {
+            System.out.println("Final queue is empty.");
+        } else {
+            System.out.println("Error? final size and isEmpty don't agree.");
+        }
+    }
+    
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            runClient(CliReader.readArgTo2DArray(args[0]));
+        } else {
+            runClient(FileReader.readLinesTo2DArray(INPUT_FILE_PATH));
+        }
+    }
+}
---
+++ java1/StackResizingArray.java
@@ -0,0 +1,108 @@
+// CLI usage: 
+// java StackLinkedList 1-2-3-777-4-5-6-777-777-777
+//
+// File usage:
+// 1 2 3 777 4 5 6 777 777 777
+
+//
+// Stack of ints implemented by linked list
+// ints in input stream will be pushed onto stack
+// input of 777 will print popped int from stack
+//
+
+package java1;
+
+import java.util.Arrays;
+
+public class StackResizingArray {
+
+    public static String INPUT_FILE_PATH = "/home/ubuntu/Dev/algs-ds/input/9stack2.txt";
+    
+    private int[] stackArray;
+    private int N = 0;
+    
+    public StackResizingArray() {
+        stackArray = new int[1];
+    }
+
+    public void push(int item) {
+        // if array is full, double its size.
+        if (N == stackArray.length) {
+            resize(2 * stackArray.length);
+        }
+        stackArray[N++] = item;
+    }
+
+    public int pop() {
+        // if array is 1/4 full, halve its size
+        if (N > 0 && N == (stackArray.length / 4)) {
+            resize(stackArray.length / 2);
+        }
+        return stackArray[--N];
+    }
+
+    private void resize(int capacity) {
+        int[] copyArray = new int[capacity];
+        for (int i = 0; i < N; i++) {
+            copyArray[i] = stackArray[i];
+        }
+        stackArray = copyArray;
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public int[] stackValues() {
+        if (isEmpty() || size() == 0) {
+            return new int[0];
+        } else {
+            int[] stackValues = new int[size()];
+            for (int i = 0; i < N; i++) {
+                stackValues[i] = stackArray[i];
+            }
+            return stackValues;
+        }
+    }
+
+    //
+    // CLIENT
+    //
+
+    private static void runClient(int[][] inputs) {
+
+        StackResizingArray sra = new StackResizingArray();
+
+        for (int i : inputs[0]) {
+            if (i == 777) {
+                System.out.println(String.format("Popped from stack: %d", sra.pop()));
+                System.out.println(String.format("Stack size: %d", sra.size()));
+            } else {
+                sra.push(i);
+                System.out.println(String.format("Pushed on: %d", i));
+                System.out.println(String.format("Stack size: %d", sra.size()));
+            }
+        }
+        int finalSize = sra.size();
+        if (finalSize > 0) {
+            System.out.println("Final stack (last-first/top-bottom/newest-oldest):");
+            System.out.println(Arrays.toString(sra.stackValues()));
+        } else if (sra.isEmpty()) {
+            System.out.println("Final stack is empty.");
+        } else {
+            System.out.println("Error? final size and isEmpty don't agree.");
+        }
+    }
+    
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            runClient(CliReader.readArgTo2DArray(args[0]));
+        } else {
+            runClient(FileReader.readLinesTo2DArray(INPUT_FILE_PATH));
+        }
+    }
+}
