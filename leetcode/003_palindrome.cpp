85526112ba3f0549e7f0ad1638b7c6431dc8a018
Brian Foster <brriann@gmail.com>
2022-04-10 20:14:34 -0600

    palindrome number cpp finished

--- leetcode/003_palindrome.cpp
+++ leetcode/003_palindrome.cpp
@@ -1,6 +1,8 @@
 #include <string>
+#include <vector>
 #include <iostream>
 
+using std::vector;
 using std::cout;
 using std::endl;
 using std::string;
@@ -9,7 +11,7 @@ using std::string;
 class Solution {
 public:
     bool isPalindrome(int x) {
-        return isPalindromeConvertToString(x);
+        return isPalindromeOptimal(x);
     }
 private:
     bool isPalindromeConvertToString(int x) {
@@ -28,8 +30,46 @@ private:
         // no inequalities were found
         return true;
     }
-    bool isPalindromeOptimized(int x) {
-        return false; // TODO
+    bool isPalindromeNoStringConvert(int x) {
+        if (x < 0) { return false; }
+
+        vector<int> numbers;
+
+        while (x > 0) {
+            numbers.push_back(x % 10);
+            x = x / 10;
+        }
+
+        vector<int>::iterator it1 = numbers.begin();
+        vector<int>::reverse_iterator it2 = numbers.rbegin();
+        for (
+            ;
+            it1 != numbers.end(), it2 != numbers.rend();
+            ++it1, ++it2
+        ) {
+            if (*it1 != *it2) {
+                return false;
+            }
+        }
+        // backwards and forwards iterators have made it through the vector<int> of digits
+        // no inequalities were found
+        return true;
+    }
+    bool isPalindromeOptimal(int x) {
+        long long int y = 0;
+        int orig = x;
+
+        // build the number backwards, digit-by-digit
+        while (x > 0) {
+            y = (y * 10) + (x % 10);
+            x = x / 10;
+        }
+
+        if (y == orig) {
+            return true;
+        }
+
+        return false;
     }
 };
 
